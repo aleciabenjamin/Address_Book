b4c6ab8e20584f24aa16ac91d6af46f3e6373bc3
Per Eriksson <per_hegg@hotmail.com>
2019-01-31 15:43:40 +0100

    adds working delete button

--- src/app.js
+++ src/app.js
@@ -12,28 +12,38 @@ const renderContacts = () => {
     
     contacts.forEach(contact => {
       let li = document.createElement('li')
+      li.id = contact.id
       li.setAttribute("class","list-reset");
       li.innerHTML = `
       <div class="card">
-      <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-      <div class="sm:flex sm:items-center px-6 py-4">
-        <div class="text-center sm:text-left sm:flex-grow">
-          <div class="content">
-            <h3 class="" >${ contact.name }</h3>
-            <h4 class="">${ contact.company }</h4>
-            <P class="">${ contact.phone }</P>
-            <p class="">${ contact.notes }</p> 
-            <p class="">${ contact.email }</p> | 
-            <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+        <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+          <div class="sm:flex sm:items-center px-6 py-4">
+            <div class="text-center sm:text-left sm:flex-grow">
+              <div class="content">
+                <h3 class="" >${ contact.name }</h3>
+                <h4 class="">${ contact.company }</h4>
+                <P class="">${ contact.phone }</P>
+                <p class="">${ contact.notes }</p> 
+                <p class="">${ contact.email }</p> | 
+                <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+                
+                
+              </div>
+            </div>
           </div>
-        </div>
       </div>
     </div>
-    </div><br>
-     `
      
+   
+    `
+    let button = document.createElement('button');
+      button.classList += "delete-contact";
+      button.innerHTML ='Delete';
+      li.appendChild(button) 
+      
       ul.appendChild(li)
-
+     
+      
     })
 
     div.appendChild(ul) 
@@ -43,55 +53,77 @@ const renderContacts = () => {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  renderContacts() 
-  const addContactForm = document.querySelector('.new-contact-form') 
-    addContactForm.addEventListener('submit', event => {
-      event.preventDefault()
-      const storage = window.localStorage
-      
-      const {
-        name,
-        email,
-        phone,
-        company,
-        notes,
-        twitter,
-      } = addContactForm.elements
-  
-      
-      const contact = {
-        id: Date.now(),
-        name: name.value,
-        email: email.value,
-        phone: phone.value,
-        company: company.value,
-        notes: notes.value,
-        twitter: twitter.value,
-      }
-  
-      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
-      let contacts = JSON.parse(storage.getItem('contacts')) || []
-      contacts.push(contact)
-      storage.setItem('contacts', JSON.stringify(contacts))
-      renderContacts() 
-      addContactForm.reset()
-    })
-      
-})
 
-let delete_button = document.querySelector('.contact-list')
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderContacts()
+
+
+  let delete_button = document.querySelector('.contact-list')
+  const storage = window.localStorage
+
+  
+
+   delete_button.addEventListener('click',event => {
+    let id = event.target.parentNode.id
+    let contacts = JSON.parse(storage.getItem('contacts')) || []
+    console.log(contacts)
+    contacts.forEach(contact => {
+      
+      contact.id == id ? contacts.splice(contacts.indexOf(contact),1)  : false
+    
+    })
+    
+    console.log(contacts)
+    
+    storage.setItem('contacts', JSON.stringify(contacts))
+    renderContacts()
+    
+    
+  }) 
+  
+  const addContactForm = document.querySelector('.new-contact-form')
+
+  
+  addContactForm.addEventListener('submit', event => {
+    
+    event.preventDefault()
 
     
+    const {
+      name,
+      email,
+      phone,
+      company,
+      notes,
+      twitter,
+    } = addContactForm.elements
 
-     delete_button.getElementById(delete_contact).addEventListener('click',event => {
-      let id = event.target.parentNode.id
-      console.log(contacts)
-      let contacts = JSON.parse(storage.getItem('contacts')) || []
-      
-      contacts.forEach(contact => {
-        
-        contact.id == id ? contacts.splice(contacts.indexOf(contact),1)  : false
-      
-      })
+    
+    const contact = {
+      id: Date.now(),
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      company: company.value,
+      notes: notes.value,
+      twitter: twitter.value,
+    }
+
+  
+  let contacts = JSON.parse(storage.getItem('contacts')) || []
+
+  contacts.push(contact)
+
+  storage.setItem('contacts', JSON.stringify(contacts))
+  
+  renderContacts()
+
+  addContactForm.reset()
+
+  })
+ 
 })
