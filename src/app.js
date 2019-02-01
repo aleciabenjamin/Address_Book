374219aa54318f535c7ef7667edc7a395d1b524f
olivia <olivia@evolve.se>
2019-02-01 12:24:41 +0100

    change sth for pull

--- src/app.js
+++ src/app.js
@@ -3,19 +3,16 @@ const renderContacts = () => {
   const contacts = JSON.parse(storage.getItem('contacts'))
 
   let div = document.querySelector('.contact-list')
-
   if (contacts) {
     div.innerHTML = '' 
     let ul = document.createElement('ul');
     ul.class = "flex";
-
     
     contacts.forEach(contact => {
       let li = document.createElement('li');
       li.id = contact.id;
       li.classList += "list-reset";
-      li.innerHTML = `
-            
+      li.innerHTML = `     
           <div class="content text-center sm:text-left sm:flex-growbg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden sm:flex sm:items-center px-6 py-4">
                 <h3 class="" >${ contact.name }</h3>
                 <h4 class="">${ contact.company }</h4>
@@ -23,10 +20,9 @@ const renderContacts = () => {
                 <p class="">${ contact.notes }</p> 
                 <p class="">${ contact.email }</p> | 
                 <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-          </div>
-        
+          </div> 
     `
-   li.appendChild(ul)
+    li.appendChild(ul)
     })
     div.appendChild(ul) 
      
